472d4c42cda2ec78ff8e7a08ed5543150b884afe
Ajith <92452173+rajkumar-hyniva@users.noreply.github.com>
2023-09-29 09:27:05 +0530

    first commit45

--- Lib.js/DeleteModule/api.js
+++ Lib.js/DeleteModule/api.js
@@ -1,6 +1,7 @@
 const {
   DynamoDBClient,
   DeleteItemCommand,
+  UpdateItemCommand,
   ConditionalCheckFailedException,
 } = require('@aws-sdk/client-dynamodb');
 
@@ -9,39 +10,41 @@ const { marshall} = require('@aws-sdk/util-dynamodb');
 const client = new DynamoDBClient();
 
 const softDeleteEmployee = async (event) => {
-  const response = { statusCode: 200 };
-  try {
-    const params = {
-      TableName: process.env.DYNAMODB_TABLE_NAME,
-      Key: marshall({ empId: event.pathParameters.empId }),
-      ConditionExpression: 'attribute_exists(empId)',
-    };
-    const deleteResult = await client.send(new DeleteItemCommand(params));
-    response.body = JSON.stringify({
-      message: 'Successfully deleted post.',
-      deleteResult,
-    });
-  } catch (e) {
+   const response = { statusCode: 200 };
+   try {
+     const empId = event.pathParameters.empId;
 
-    if (error instanceof ConditionalCheckFailedException) {
-       console.error(e);
-       response.statusCode = 401;
-       response.body = JSON.stringify({
-         message: 'empId not found',
-         errorMsg: e.message,
-         errorStack: e.stack,
-       }); 
-    } else {
-      console.error(e);
-      response.statusCode = 500;
-      response.body = JSON.stringify({
-        message: 'Failed to delete post.',
-        errorMsg: e.message,
-        errorStack: e.stack,
-      });
-    }
-  }
-  return response;
+     // Define the update expression to set isActive to true
+     const updateExpression = 'SET isActive = :isActive';
+
+     // Define the expression attribute values
+     const expressionAttributeValues = marshall({
+       ':isActive': true,
+     });
+
+     const params = {
+       TableName: process.env.DYNAMODB_TABLE_NAME,
+       Key: marshall({ empId: empId }),
+       UpdateExpression: updateExpression,
+       ExpressionAttributeValues: expressionAttributeValues,
+     };
+
+     const updateResult = await client.send(new UpdateItemCommand(params));
+
+     response.body = JSON.stringify({
+       message: 'Employee activated successfully.',
+       updateResult,
+     });
+   } catch (e) {
+     console.error(e);
+     response.statusCode = 500;
+     response.body = JSON.stringify({
+       message: 'Failed to activate employee.',
+       errorMsg: e.message,
+       errorStack: e.stack,
+     });
+   }
+   return response;
 };
 
 const deleteEmployee = async (event) => {
